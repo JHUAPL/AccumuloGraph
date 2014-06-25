872870a6628f1be76eafcbb98eb5c58f1e12da8b
Michael Lieberman <mike.d.lieberman@gmail.com>
2014-06-24 20:34:42 -0400

    Added graph test suite

--- src/test/java/edu/jhuapl/tinkerpop/AccumuloGraphTestSuite.java
+++ src/test/java/edu/jhuapl/tinkerpop/AccumuloGraphTestSuite.java
@@ -3,7 +3,17 @@
  */
 package edu.jhuapl.tinkerpop;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.apache.hadoop.io.Text;
+
+import com.tinkerpop.blueprints.Graph;
+import com.tinkerpop.blueprints.GraphFactory;
 import com.tinkerpop.blueprints.TestSuite;
+
+import edu.jhuapl.tinkerpop.AccumuloGraphConfiguration.InstanceType;
 
 /**
  * @author Michael Lieberman (http://mikelieberman.org)
@@ -11,4 +21,47 @@ import com.tinkerpop.blueprints.TestSuite;
  */
 public class AccumuloGraphTestSuite extends TestSuite {
 
+	public void testSplits() throws Exception {
+		AccumuloGraphConfiguration cfg;
+
+		cfg = getGraphConfig("nullSplits").splits((String) null);
+		Graph graph = GraphFactory.open(cfg);
+		for (String table : cfg.getTableNames()) {
+			assertEquals(0, cfg.getConnector().tableOperations().listSplits(table).size());
+		}
+		graph.shutdown();
+
+		cfg = getGraphConfig("emptySplits").splits("");
+		graph = GraphFactory.open(cfg);
+		for (String table : cfg.getTableNames()) {
+			assertEquals(0, cfg.getConnector().tableOperations().listSplits(table).size());
+		}
+		graph.shutdown();
+
+		cfg = getGraphConfig("threeSplits").splits(" a b c ");
+		graph = GraphFactory.open(cfg);
+		for (String table : cfg.getTableNames()) {
+			Collection<Text> splits = cfg.getConnector().tableOperations().listSplits(table);
+			assertEquals(3, splits.size());
+			List<Text> arr = new ArrayList<Text>(splits);
+			assertEquals("a", arr.get(0).toString());
+			assertEquals("b", arr.get(1).toString());
+			assertEquals("c", arr.get(2).toString());
+		}
+		graph.shutdown();
+	}
+
+	//public void testSplitsArray() {
+	//	AccumuloGraphConfiguration cfg = getGraphConfig("testSplitsArray");		
+	//}
+
+	private AccumuloGraphConfiguration getGraphConfig(String graphDirectoryName) {
+		AccumuloGraphConfiguration cfg = new AccumuloGraphConfiguration();
+		cfg.instance("instanceName").zkHosts("ZookeeperHostsString");
+		cfg.user("root").password("".getBytes());
+		cfg.name(graphDirectoryName).create(true).autoFlush(true)
+				.instanceType(InstanceType.Mock).lruMaxCapacity(10)
+				.propertyCacheTimeout(10000);
+		return cfg;
+	}
 }
