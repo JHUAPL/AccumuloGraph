ed0fc7184a151600e349f8107eaee755a073ce59
webbrl1 <Ryan.Webb@jhuapl.edu>
2014-07-17 20:45:03 -0400

    Added tests to implictly test caching and timeouts.
    We should still add test for explicitly testing them

---
+++ src/test/java/edu/jhuapl/tinkerpop/ExtendedAccumuloGraphTest.java
@@ -0,0 +1,17 @@
+package edu.jhuapl.tinkerpop;
+
+import com.tinkerpop.blueprints.Graph;
+import com.tinkerpop.blueprints.GraphFactory;
+
+public class ExtendedAccumuloGraphTest extends AccumuloGraphTest {
+	@Override
+	public Graph generateGraph(String graphDirectoryName) {
+		AccumuloGraphConfiguration cfg =
+				AccumuloGraphTestUtils.generateGraphConfig(graphDirectoryName);
+		cfg.setLruMaxCapacity(20).setPreloadedProperties(new String[]{"test"})
+			.setPreloadedEdgeLabels(new String[]{"knows"})
+			.setPropertyCacheTimeout(100000).skipExistenceChecks(true);
+		testGraphName.set(graphDirectoryName);
+		return GraphFactory.open(cfg.getConfiguration());
+	}
+}
