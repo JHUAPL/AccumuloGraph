33042acedc2b929a72e4cc1dafd4f72080358f32
Michael Lieberman <Michael.Lieberman@jhuapl.edu>
2015-01-05 19:28:38 -0500

    Fix header licenses

--- src/main/java/edu/jhuapl/tinkerpop/mutator/vertex/BaseVertexMutator.java
+++ src/main/java/edu/jhuapl/tinkerpop/mutator/vertex/BaseVertexMutator.java
@@ -1,14 +1,17 @@
-/******************************************************************************
- *                              COPYRIGHT NOTICE                              *
- * Copyright (c) 2014 The Johns Hopkins University/Applied Physics Laboratory *
- *                            All rights reserved.                            *
- *                                                                            *
- * This material may only be used, modified, or reproduced by or for the      *
- * U.S. Government pursuant to the license rights granted under FAR clause    *
- * 52.227-14 or DFARS clauses 252.227-7013/7014.                              *
- *                                                                            *
- * For any other permissions, please contact the Legal Office at JHU/APL.     *
- ******************************************************************************/
+/* Copyright 2014 The Johns Hopkins University Applied Physics Laboratory
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package edu.jhuapl.tinkerpop.mutator.vertex;
 
 import com.tinkerpop.blueprints.Vertex;
